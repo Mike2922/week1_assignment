b17edab84855445c87a4339043007cf01a1a321e
Mike2922 <michaelpaz22@gmail.com>
2022-11-07 21:44:46 -0800

    second commit

---
+++ learn.java
@@ -0,0 +1,26 @@
+package conditionals;
+
+public class Week2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String  name = "Sam" ;
+      if (name=="Sam") {
+    	  System.out.println("Hello Sam!");
+      } else {
+    	  System.out.println("You are not sam");
+    	  
+      }
+      
+      double costOfMilk = 5; 
+      if (costOfMilk <= 5) {
+    	  System.out.println("im buying 3 units");
+      } else if (costOfMilk >= 5 ) {
+    	  System.out.println(" i cant afford it");
+      } else {
+    	  System.out.println("i can buy some" ); 
+      }
+	}
+
+}
+
